1b7e86868521cb2e2ce50263e29db1f8ac553549
Hoa0304 <tranthicamhoa2k4@gmail.com>
2024-08-18 16:28:42 +0700

    #4: constructor(super)

---
+++ src/Component life cycles/constructor.jsx
@@ -0,0 +1,9 @@
+class App extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.DEFAULT_MAX_LENGTH = 10;
+        this.state = {
+            productList: [],
+        };
+    }
+}
